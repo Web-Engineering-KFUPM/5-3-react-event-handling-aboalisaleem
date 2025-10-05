84b1bf5a165461bfcad6ef1f8e5cc696ae109851
aboalisaleem <hadisaleem50@gmail.com>
2025-10-05 11:49:36 +0300

    task 2&3

--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -5,9 +5,12 @@ export default function TaskList({ tasks, onDelete }) {
   return (
     <ul className="list">
       {/* Task 2 – Display Placeholder if No Tasks Yet */}
+      {tasks.length === 0 && <p className="placeholder">No tasks yet!</p>}
 
       {/* Task 2 & 3 – Map tasks to TaskItem */}
-      
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} onDelete={onDelete} />
+      ))}
     </ul>
   );
 }
