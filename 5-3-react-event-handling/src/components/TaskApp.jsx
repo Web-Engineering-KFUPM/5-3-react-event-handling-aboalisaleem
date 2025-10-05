8578ec3c26c8f0f007da298df2b26c1c67c39629
aboalisaleem <hadisaleem50@gmail.com>
2025-10-05 11:47:27 +0300

    finsh todo

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -10,11 +10,13 @@ export default function TaskApp() {
   
   const handleDelete = (id) => {
     // TODO: filter tasks by id to remove the clicked one
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   
   const handleClearAll = () => {
     // TODO: set tasks to empty array
+    setTasks([]);
   };
 
   return (
@@ -26,7 +28,9 @@ export default function TaskApp() {
           placeholder="Type a task..."
           className="input"
           // TODO: value={text}
+          value={text}
           // TODO: onChange={(e) => setText(e.target.value)}
+          onChange={(e) => setText(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSubmit();
           }}
@@ -39,6 +43,7 @@ export default function TaskApp() {
       {/*Render Task List and Enable Delete */}
       {/*Pass tasks and onDelete */}
       <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+
 
       {/*Clear All */}
       <div className="footerRow">
